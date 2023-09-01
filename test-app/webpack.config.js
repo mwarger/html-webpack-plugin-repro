42b7a524afbc2acef4a18c041832c5800f037de0
Mat Warger <686823+mwarger@users.noreply.github.com>
2023-09-01 13:59:41 -0600

    added example

---
+++ test-app/webpack.config.js
@@ -0,0 +1,30 @@
+const { composePlugins, withNx } = require('@nx/webpack');
+const { withReact } = require('@nx/react');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Nx plugins for webpack.
+module.exports = composePlugins(withNx(), withReact(), (config) => {
+  // write config to file
+  const fs = require('fs');
+  fs.writeFileSync('webpack.config.json', JSON.stringify(config, null, 2));
+
+  // Update the webpack config as needed here.
+  // e.g. `config.plugins.push(new MyPlugin())`
+  config.plugins.push(
+    ...[
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        templateParameters: {
+          mything: 'mything',
+        },
+      }),
+    ]
+  );
+
+  fs.writeFileSync(
+    'webpack-updated.config.json',
+    JSON.stringify(config, null, 2)
+  );
+
+  return config;
+});
